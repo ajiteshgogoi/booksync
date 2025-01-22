2cb2b03cf23ea0e1ca5b14200321f5f53ce526cf
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-01-22 14:49:52 +0530

    Improve UI

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,15 @@ module.exports = {
         secondary: '#4f46e5',
         success: '#10b981',
         error: '#ef4444'
+      },
+      animation: {
+        slide: 'slide 2s cubic-bezier(0.4, 0, 0.6, 1) infinite'
+      },
+      keyframes: {
+        slide: {
+          '0%, 100%': { transform: 'translateX(-10%)' },
+          '50%': { transform: 'translateX(10%)' }
+        }
       }
     },
   },
