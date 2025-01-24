5ab28b5a036f558e0b7f004dac62a3dcf9f3d1e2
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-01-25 05:13:52 +0530

    Update redisUtils.ts

--- server/src/utils/redisUtils.ts
+++ server/src/utils/redisUtils.ts
@@ -1,5 +1,5 @@
 import { Redis } from 'ioredis';
-import { getRedis } from '../services/redisService';
+import { getRedis } from '../services/redisService.js';
 
 interface RedisConnectionOptions {
   maxRetries?: number;
