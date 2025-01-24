1c7a49a72f233da32971839d7101c2f8f32475fa
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-01-24 18:08:03 +0530

    Revert "Update redisServiceFactory.ts"
    This reverts commit 59cfe53e1edabc5600fa4b2fe0c3dd3168ef8b6d.

--- server/src/services/redisServiceFactory.ts
+++ server/src/services/redisServiceFactory.ts
@@ -3,7 +3,7 @@ import { vercelRedisService } from './vercelRedisService.js';
 
 class RedisServiceFactory {
   static getService() {
-    if (process.env.VERCEL || process.env.NODE_ENV === 'production') {
+    if (process.env.VERCEL) {
       return vercelRedisService;
     }
     return RedisPool.getInstance();
