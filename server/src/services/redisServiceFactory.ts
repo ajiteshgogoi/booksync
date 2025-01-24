59cfe53e1edabc5600fa4b2fe0c3dd3168ef8b6d
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-01-24 17:30:39 +0530

    Update redisServiceFactory.ts

--- server/src/services/redisServiceFactory.ts
+++ server/src/services/redisServiceFactory.ts
@@ -3,7 +3,7 @@ import { vercelRedisService } from './vercelRedisService.js';
 
 class RedisServiceFactory {
   static getService() {
-    if (process.env.VERCEL) {
+    if (process.env.VERCEL || process.env.NODE_ENV === 'production') {
       return vercelRedisService;
     }
     return RedisPool.getInstance();
