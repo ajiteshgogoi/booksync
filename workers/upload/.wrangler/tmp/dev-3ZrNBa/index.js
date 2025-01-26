3acba60b0dd31233e2afa49daac5895834cf907e
ajitesh gogoi <136594063+ajiteshgogoi@users.noreply.github.com>
2025-01-26 14:48:51 +0530

    Revert "Fix wrangler values"
    This reverts commit c7c350b740291d7bdb6ffe7e45e0f542110a9cc1.

--- workers/upload/.wrangler/tmp/dev-3ZrNBa/index.js
+++ workers/upload/.wrangler/tmp/dev-3ZrNBa/index.js
@@ -19483,7 +19483,7 @@ var require_dist = __commonJS({
   }
 });
 
-// .wrangler/tmp/bundle-NJ3g0v/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-Mx0iwq/middleware-loader.entry.ts
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_process();
 init_virtual_unenv_global_polyfill_performance();
@@ -19491,7 +19491,7 @@ init_virtual_unenv_global_polyfill_console();
 init_virtual_unenv_global_polyfill_set_immediate();
 init_virtual_unenv_global_polyfill_clear_immediate();
 
-// .wrangler/tmp/bundle-NJ3g0v/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-Mx0iwq/middleware-insertion-facade.js
 init_modules_watch_stub();
 init_virtual_unenv_global_polyfill_process();
 init_virtual_unenv_global_polyfill_performance();
@@ -20000,7 +20000,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env3, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-NJ3g0v/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-Mx0iwq/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -20038,7 +20038,7 @@ function __facade_invoke__(request, env3, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-NJ3g0v/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-Mx0iwq/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
